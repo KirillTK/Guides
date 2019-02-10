7f5133a60e02d6dcfd51d05858e90ec365b017e4
Kirill Tkachov <35868628+KirillTK@users.noreply.github.com>
2019-02-10 22:26:24 +0300

    feature: add routing to login/reg pages

--- front/src/app/shared/modules/AppRoutingModule.ts
+++ front/src/app/shared/modules/AppRoutingModule.ts
@@ -1,10 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LandingPageComponent} from '../../pages/langing-page/landing-page';
+import {LoginPageComponent} from '../../pages/login-page/login-page';
+import {RegistrationPageComponent} from '../../pages/registration-page/registration-page';
 
 
 const routes: Routes = [
-  {path: '', component: LandingPageComponent}
+  {path: '', component: LandingPageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'registration', component: RegistrationPageComponent}
   // {path: 'film/:id', component: FilmComponent}
 ];
 
