dac65091e4dedb9dfc50977569374ade116ff7bf
Kirill Tkachov <35868628+KirillTK@users.noreply.github.com>
2019-02-11 00:03:16 +0300

    feature: create write instruction component

---
+++ front/src/app/pages/user-page/components/write-instruction/write-instruction.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-write-instruction',
+  templateUrl: './write-instruction.component.html',
+  styleUrls: ['./write-instruction.component.css']
+})
+export class WriteInstructionComponent {
+}
