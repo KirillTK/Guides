319f5644af752e116a1dd8369e93aac83777eefd
Kirill Tkachov <35868628+KirillTK@users.noreply.github.com>
2019-02-25 23:41:08 +0300

    front-feature: create settings page

---
+++ front/src/app/pages/setting-page/setting-page.component.ts
@@ -0,0 +1,26 @@
+import {Component, OnInit} from '@angular/core';
+import {OverlayContainer} from '@angular/cdk/overlay';
+import {ThemeService} from '../../shared/services/Theme.service';
+import {Observable} from 'rxjs';
+
+@Component({
+  selector: 'app-setting-page',
+  templateUrl: './setting-page.component.html',
+  styleUrls: ['./setting-page.component.scss']
+})
+export class SettingPageComponent implements OnInit {
+
+  isDarkTheme: Observable<boolean>;
+
+  constructor(private themeService: ThemeService) { }
+
+  ngOnInit() {
+    this.isDarkTheme = this.themeService.isDarkTheme;
+  }
+
+  toggleDarkTheme(checked: boolean) {
+    this.themeService.setDarkTheme(checked);
+  }
+
+
+}
