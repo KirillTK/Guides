860d8567e911f77e06245063dc95dc8ccedd927c
KirillTK <kirill99.tkachev@yandex.by>
2019-03-06 01:53:00 +0300

    back-feature: update guard admin

--- back/api/admin.js
+++ back/api/admin.js
@@ -5,50 +5,32 @@ const {guardAdminApi} = require('./guard');
 
 
 router.get('/api/admin/getListUsers', guardAdminApi, async (req, res) => {
+  console.log('here');
   const users = await User.find({});
   res.json(users);
-  // res.json({status: false});
-  // }
 });
 
-router.delete('/api/admin/deleteUser/:uid', async (req, res) => {
-  if (req.session.user) {
-    if (req.session.user.isAdmin) {
-      const uid = req.params.uid;
-      await User.deleteOne({_id: uid});
-      const users = await User.find({});
-      res.json(users);
-    } else res.json({status: false});
-  } else {
-    res.json({status: false});
-  }
+router.delete('/api/admin/deleteUser/:uid', guardAdminApi, async (req, res) => {
+  const uid = req.params.uid;
+  await User.deleteOne({_id: uid});
+  const users = await User.find({});
+  res.json(users);
+
 });
 
-router.put('/api/admin/updateRole', async (req, res) => {
-  if (req.session.user) {
-    if (req.session.user.isAdmin) {
-      const {_id} = req.body;
-      await User.updateOne({_id: _id}, req.body);
-      const users = await User.find({});
-      res.json(users);
-    } else res.json({status: false});
-  } else {
-    res.json({status: false});
-  }
+router.put('/api/admin/updateRole', guardAdminApi, async (req, res) => {
+  const {_id} = req.body;
+  await User.updateOne({_id: _id}, req.body);
+  const users = await User.find({});
+  res.json(users);
 });
 
 
-router.put('/api/admin/blockUser', async (req, res) => {
-  if (req.session.user) {
-    if (req.session.user.isAdmin) {
-      const {_id} = req.body;
-      await User.updateOne({_id: _id}, req.body);
-      const users = await User.find({});
-      res.json(users);
-    } else res.json({status: false});
-  } else {
-    res.json({status: false});
-  }
+router.put('/api/admin/blockUser', guardAdminApi, async (req, res) => {
+  const {_id} = req.body;
+  await User.updateOne({_id: _id}, req.body);
+  const users = await User.find({});
+  res.json(users);
 });
 
 module.exports = router;
