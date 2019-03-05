88fe80e2004dd1e9060ec632d1c61dd0e6a950f8
KirillTK <kirill99.tkachev@yandex.by>
2019-03-06 02:44:04 +0300

    front-feature: guard route

--- front/src/app/shared/guards/AdminPageRouteGuard.ts
+++ front/src/app/shared/guards/AdminPageRouteGuard.ts
@@ -15,21 +15,15 @@ export class AdminPageRouteGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
 
-    // if (this.auth.isLoggedIn && this.user.user._id === next.params.id && this.user.user.isAdmin) {
-    //   return true;
-    // }
-    //
-    // return this.user.isLoggedIn().pipe(map(res => {
-    //   if (res.status && res.user.isAdmin) {
-    //     this.auth.setLoggedIn(true);
-    //     this.user.user = res.user;
-    //     this.auth.token = res.token;
-    //     return true;
-    //   } else {
-    //     this.router.navigate(['login']);
-    //     return false;
-    //   }
-    // }));
-    return true;
+    return this.user.isLoggedIn().pipe(map(res => {
+      if (res.status && (next.params.id === res.user._id && res.user.isAdmin)) {
+        this.auth.setLoggedIn({isAuth: res.status, user: res.user});
+        this.user.user = res.user;
+        return true;
+      } else {
+        this.router.navigate(['/']);
+        return false;
+      }
+    }));
   }
 }
--- front/src/app/shared/guards/UserPageRouteGuard.ts
+++ front/src/app/shared/guards/UserPageRouteGuard.ts
@@ -14,26 +14,16 @@ export class UserPageRouteGuard implements CanActivate {
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    // if (!this.auth.isLoggedIn) {
-    //   this.router.navigate(['/login']);
-    // }
 
-    // if ((this.auth.isLoggedIn && this.user.user._id === next.params.id) || (this.auth.isLoggedIn && this.user.user.isAdmin)) {
-    //   return true;
-    // }
-    //
-    // return this.user.isLoggedIn(next.params.id).pipe(map(res => {
-    //   console.log(res);
-    //   if (res.status) {
-    //     this.auth.setLoggedIn(true);
-    //     this.user.user = res.user;
-    //     this.auth.token = res.token;
-    //     return true;
-    //   } else {
-    //     this.router.navigate(['login']);
-    //     return false;
-    //   }
-    // }));
-    return true;
+    return this.user.isLoggedIn().pipe(map(res => {
+      if (res.status && (next.params.id === res.user._id || res.user.isAdmin)) {
+        this.auth.setLoggedIn({isAuth: res.status, user: res.user});
+        this.user.user = res.user;
+        return true;
+      } else {
+        this.router.navigate(['/']);
+        return false;
+      }
+    }));
   }
 }
