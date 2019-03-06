3bbf6f0290128e44fcec5adc9e31d4bfd81cad3e
KirillTK <kirill99.tkachev@yandex.by>
2019-03-06 20:54:43 +0300

    back-fix: add russian language support in generate pdf scripts

--- back/scripts/generatePDF.js
+++ back/scripts/generatePDF.js
@@ -3,25 +3,26 @@ const PDFDocument = require('pdfkit');
 const generatePDF = (instruction, response) => {
   const doc = new PDFDocument;
   doc.pipe(response);
-  doc.font('Times-Roman')
+  doc.registerFont('NotoSans', __dirname + '\\fonts\\NotoSans.ttf');
+  doc.font('NotoSans')
     .fontSize(26)
     .text(instruction.name, {align: 'center'});
   doc.moveDown();
-  doc.font('Times-Roman')
+  doc.font('NotoSans')
     .fontSize(20)
     .text(`Author: ${instruction.author}`);
-  doc.font('Times-Roman')
+  doc.font('NotoSans')
     .fontSize(12)
     .text(`Theme: ${instruction.theme}`);
-  doc.font('Times-Roman')
+  doc.font('NotoSans')
     .fontSize(12)
     .text(`Score: ${instruction.score}`);
   doc.moveDown();
   instruction.steps.forEach((step, index) => {
-    doc.font('Times-Roman')
+    doc.font('NotoSans')
       .fontSize(12)
       .text(`Step ${index + 1}: ${step.stepTitle}`);
-    doc.font('Times-Roman')
+    doc.font('NotoSans')
       .fontSize(10)
       .text(step.descriptionTitle);
     doc.moveDown();
